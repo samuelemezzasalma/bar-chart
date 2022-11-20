fb2c34bf7ea243b22c8e8f992866bedb8fed004c
samuelemezzasalma <samuele.mezza@gmail.com>
2022-11-20 07:46:19 +0100

    bar-chart finally diplayed

--- src/lib/components/BarChart.svelte
+++ src/lib/components/BarChart.svelte
@@ -1,34 +1,65 @@
 <script>
-
-	import * as d3 from "d3";
+       import { onMount } from 'svelte';
+	import * as d3 from 'd3';
 
 	export let allGdpData;
+       const dataset = allGdpData.data
 
-	const width = 800;
-	const height = 600;
+	const w = 800;
+	const h = 600;
+       const padding = 60;
 
-	const svg = d3.select("svg")
-	svg.selectAll("rect")
-       .data(allGdpData.data)
-       .enter()
-       .append("rect")
-       .attr("x", (d, i) => i * 10)
-       .attr("y", (d, i) => height - 3 * d[1])
-       .attr("width", 5)
-       .attr("height", (d, i) => d[1] * 3)
-       .attr("fill", "navy")
-       .attr("class", "bar")
+       let el;
+
+	onMount(() => {
+              const yScale = d3.scaleLinear()
+              .domain([d3.min(dataset, (d) => d[1]), d3.max(dataset, (d) => d[1])])
+              .range([h - padding, padding]);
+
+       
+
+              const svg = d3.select(el)
+                     .append("svg")
+                     .attr("width", w)
+                     .attr("height", h);
+	
+              svg.selectAll("rect")
+              .data(dataset)
+              .enter()
+              .append("rect")
+              .attr("x", (d, i) => i * 10)
+              .attr("y", (d, i) => h - (d[1] / 2))
+              .attr("width", 5)
+              .attr("height", (d, i) => d[1] / 2)
+              .attr("fill", "navy")
+              .attr("class", "bar")
+	});
+
+//     const xScale = d3.scaleLinear()
+//                      .domain([0, d3.max(dataset, (d) => d[0])])
+//                      .range([padding, w - padding]);
+
+    
 
 </script>
 
-<!-- {allGdpData} -->
+<!-- {JSON.stringify(dataset)}  -->
 
-<svg x="400" y="400" {width} {height}>
+<!-- x="400" y="400" -->
 
-</svg>
+<div bind:this={el} class="chart"></div>
+
+<!-- <svg  {w} {h}>
+
+</svg> -->
 
 <style>
-/* svg :global(rect.bar):hover {
-	fill: brown;
-} */
+.chart :global(div) {
+		font: 10px sans-serif;
+		background-color: steelblue;
+		text-align: right;
+		padding: 3px;
+		margin: 1px;
+		color: white;
+	}
 </style>
