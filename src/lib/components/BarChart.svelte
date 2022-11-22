ba03a36a03df60b09893babc9d7284ec401b8fc4
samuelemezzasalma <samuele.mezza@gmail.com>
2022-11-22 14:12:39 +0100

    1 file modified

--- src/lib/components/BarChart.svelte
+++ src/lib/components/BarChart.svelte
@@ -10,28 +10,27 @@
        const padding = 15;
 
        let el;
-       console.log(el)
-       console.log(dataset.length)
-       console.log(w)
+       // console.log(el)
+       // console.log(dataset.length)
+       // console.log(w)
 
-       const yScale = d3.scaleLinear()
+       function drawChart() {
+              const yScale = d3.scaleLinear()
               .domain([0, d3.max(dataset, (d) => d[1])])
               .range([h - padding, 0]);
-              
+                     
               const xScale = d3.scaleLinear()
               .domain([1, dataset.length * 2])
               .range([padding, w - padding]);
 
-       
-
               const svg = d3.select(el)
-                     .append("svg")
-                     .attr("preserveAspectRatio", "xMinYMin meet")
-                     .attr("viewBox", `0 0 ${w} ${h}`)
-                     .attr("width", w)
-                     .attr("height", h)
-                     .attr("class", "svg-content-responsive");
-	
+              .append("svg")
+              .attr("preserveAspectRatio", "xMinYMin meet")
+              .attr("viewBox", `0 0 ${w} ${h}`)
+              .attr("width", w)
+              .attr("height", h)
+              .attr("class", "svg-content-responsive");
+              
               svg.selectAll("rect")
               .data(dataset)
               .enter()
@@ -42,28 +41,28 @@
               .attr("height", (d, i) => yScale(h - d[1]))
               .attr("fill", "navy")
               .attr("class", "bar")
+       }
 
-       function drawChart() {
+       function adaptChart() {
+              console.log("inside")
               // get the current width of the div where the chart appear, and attribute it to Svg
-              currentWidth = parseInt(d3.select('.chart').style('width'), 10)
+              const currentWidth = parseInt(d3.select('.chart').style('width'), 10)
               svg.attr("width", currentWidth)
 
               // Update the X scale and Axis (here the 20 is just to have a bit of margin)
               xScale.range([padding, currentWidth - padding]);
               // xAxis.call(d3.axisBottom(x))
-
-              // Add the last information needed for the circles: their X position
-              // myCircles
-              // .attr("cx", function(d){ return x(d)})
        }
-
-       // Add an event listener that run the function when dimension change
-       window.addEventListener('resize', drawChart );
 
 
 
 	onMount(() => {
-              drawChart()
+               // Add an event listener that run the function when dimension change
+              window.addEventListener('resize', adaptChart );
+              drawChart();
+
+              return () => window.removeEventListener('resize', adaptChart);
+
 	});
 
     
@@ -75,23 +74,23 @@
 
 
 <style>
-svg {
-       border: 1px solid black;
-}
+       svg {
+              border: 1px solid black;
+       }
 
-.chart {
-       display: inline-block;
-       position: relative;
-       width: 100%;
-       padding-bottom: 100%; /* aspect ratio */
-       vertical-align: top;
-       overflow: hidden;
-}
+       .chart {
+              display: inline-block;
+              position: relative;
+              width: 100%;
+              padding-bottom: 100%; /* aspect ratio */
+              vertical-align: top;
+              /* overflow: hidden; */
+       }
 
-.svg-content-responsive {
-  display: inline-block;
-  position: absolute;
-  top: 10px;
-  left: 0;
-}
+       .svg-content-responsive {
+              display: inline-block;
+              position: absolute;
+              top: 10px;
+              left: 0;
+       }
 </style>
