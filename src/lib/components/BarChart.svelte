d7cdd85f7f0904ee641acc637a691ccfec3c3c77
samuelemezzasalma <samuele.mezza@gmail.com>
2022-11-22 10:40:17 +0100

    1 file modified

--- src/lib/components/BarChart.svelte
+++ src/lib/components/BarChart.svelte
@@ -5,8 +5,8 @@
 	export let allGdpData;
        const dataset = allGdpData.data
 
-	const w = 800;
-	const h = 600;
+	const w = 1200;
+	const h = 800;
        const padding = 15;
 
        let el;
@@ -20,24 +20,26 @@
               .range([h - padding, 0]);
               
               const xScale = d3.scaleLinear()
-              .domain([1, dataset.length])
+              .domain([1, dataset.length * 2])
               .range([padding, w - padding]);
 
        
 
               const svg = d3.select(el)
                      .append("svg")
+                     .attr("preserveAspectRatio", "xMinYMin meet")
+                     .attr("viewBox", `0 0 ${w} ${h}`)
                      .attr("width", w)
                      .attr("height", h)
-                     .attr("class", "test");
+                     .attr("class", "svg-content-responsive");
 	
               svg.selectAll("rect")
               .data(dataset)
               .enter()
               .append("rect")
-              .attr("x", (d, i) => xScale(i))
+              .attr("x", (d, i) => xScale(i * 2))
               .attr("y", (d, i) => yScale(d[1]))
-              .attr("width", 2)
+              .attr("width", 1)
               .attr("height", (d, i) => yScale(h - d[1]))
               .attr("fill", "navy")
               .attr("class", "bar")
@@ -63,13 +65,19 @@ svg {
        border: 1px solid black;
 }
 
-.chart  {
-              /* border: 1px solid black; */
-		font: 10px sans-serif;
-		/* background-color: steelblue; */
-		text-align: right;
-		/* padding: 3px; */
-		margin: 1px;
-		color: white;
-	}
+.chart {
+       display: inline-block;
+       position: relative;
+       width: 100%;
+       padding-bottom: 100%; /* aspect ratio */
+       vertical-align: top;
+       overflow: hidden;
+}
+
+.svg-content-responsive {
+  display: inline-block;
+  position: absolute;
+  top: 10px;
+  left: 0;
+}
 </style>
